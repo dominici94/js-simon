6693e809aaad5308f821602173d8d607d8cc4968
dominici94 <mattiadominici94@gmail.com>
2021-11-24 16:38:22 +0100

    esercizio

--- script.js
+++ script.js
@@ -8,8 +8,6 @@
 
 // genera 5 numeri casuali da 1 a 100
 
-
-
 const container = document.querySelector('.container');
 const numeri = generaRandom();
 
@@ -22,7 +20,6 @@ function generaRandom(){
     const arrayNumber = [];
     while(arrayNumber.length < 5){
         const number = Math.round(Math.random()*99) + 1;
-        console.log(number);
         if(!arrayNumber.includes(number)){
             arrayNumber.push(number);
         }
@@ -34,6 +31,39 @@ function generaRandom(){
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// setTimeout(funzione da attivare dopo 30 sec,30000){
+setTimeout(play, 3000);
 
-// }
+// questa funzione da attivare deve fare inserire i 5 numeri visti col prompt all'utente. 
+
+function play(){
+
+    const arrayTipo = getUserNumbers();
+
+    function getUserNumbers(){
+        const arrayUser = [];
+        while(arrayUser.length < 5){
+            const number = parseInt(prompt('inserisci i numeri visti prima uno alla volta.'));
+            arrayUser.push(number);
+        }
+        return arrayUser;
+    }
+
+    let count = 0;
+    const rightArray = [];
+
+    for(let i=0; i<5; i++){
+        if(numeri.includes(arrayTipo[i])){
+            count++;
+            rightArray.push(arrayTipo[i]);
+        }
+    }
+    container.innerHTML = `
+        Hai indovinato ${count} numeri:
+        ${rightArray}.
+    `;
+}
+
+
+
+
+
